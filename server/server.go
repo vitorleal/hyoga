f4a40e98c15f42ef407afd256e06bb147a5826a4
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-06-03 16:14:31 -0300

    psql connection fixed
    pq dependency vendored
    
    hstore dependency vendored
    
    oid dependency vendored
    
    mapstructure dependency vendored
    
    get env port

--- server/server.go
+++ server/server.go
@@ -2,15 +2,15 @@ package server
 
 import(
     "log"
+    "os"
     "net/http"
-    "github.com/fiscaluno/hyoga/config"
     "github.com/fiscaluno/hyoga/server/routes"
     _ "github.com/fiscaluno/hyoga/database/migrations"
-    
+
 )
 
 func Start() {
-    port := config.DEV_SERVER_PORT
+    port := os.Getenv("PORT")
     router := routes.GetRouter()
 
     if err := http.ListenAndServe(":" + port, router); err != nil {
